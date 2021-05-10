faa297296c6dff178dfdc2194cc87fdd1c40c37a
hanh <hanchan1305@gmail.com>
2021-05-10 23:17:50 +0900

    update sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./scss/App.scss";
 import DaysBar from "../components/DaysBar";
 import HoursBar from "../components/HoursBar";
 import MainDisplay from "../components/MainDisplay";
