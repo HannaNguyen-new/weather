e80da7fc6808c58d45b78714d98e9771c126e8b1
nguyen <>
2021-05-11 07:30:09 +0900

    update sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./scss/App.scss";
-import DaysBar from "../components/DaysBar";
-import HoursBar from "../components/HoursBar";
-import MainDisplay from "../components/MainDisplay";
-import SearchBar from "../components/SearchBar";
-import SearchHistory from "../components/SearchHistory";
-import WeatherCard from "../components/WeatherCard";
+import DaysBar from "./components/DaysBar";
+import HoursBar from "./components/HoursBar";
+import MainDisplay from "./components/MainDisplay";
+import SearchBar from "./components/SearchBar";
+import SearchHistory from "./components/SearchHistory";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
   return (
