ea5fb3e855afb723f01a82ef73279ca993b96e15
nguyen <>
2021-05-12 08:01:21 +0900

    update sass

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,28 +1,11 @@
 import React from "react"
 
 function WeatherCard() {
-   const style = {
-      maxWidth: "340px",
-      maxHeight: "170px",
-      color: "#FFFFFF",
-      textShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
-      textAlign: "center",
-      location:{
-         fontSize: "40px",
-      },
-      status:{
-         fontSize: "18px",
-         marginBottom: "5px"
-      },
-      temp:{
-         fontSize: "55px"
-      }
-   }
    return (
-      <div style={style}>
-         <h2 style={style.location}>Fukuoka</h2>
-         <p style={style.status}>Rain</p>
-         <h1 style={style.temp}>19</h1>
+      <div className='weatherCard'>
+         <h2 className='location'>Fukuoka</h2>
+         <p className='status'>Rain</p>
+         <h1 className='temp'>19</h1>
       </div>
    )
 }
