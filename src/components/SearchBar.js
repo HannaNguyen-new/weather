0363ad833492ebbc029725edfb77150757508122
hanh <hanchan1305@gmail.com>
2021-05-16 21:39:38 +0900

    edit searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React from "react";
 
 function SearchBar() {
+
+//   const hour = new Date()
+//   const seconds = Date.UTC(hour)
+//   const now = new Date().toLocaleString('en-GB', { timeZone: 'UTC' }).split(",")
+//   const hour = new Date().toLocaleTimeString("ja-JP").split(":")
+//  // const utc = Date.UTC(year,month,date,hour-9)
+
+
+  const search = (event) => {
+    const geoUrl =
+      "http://api.openweathermap.org/geo/1.0/direct?q=fukuoka&appid=b215978ae0b5adea831c87cd99ac6d51";
   
-  return <div className="searchBar"></div>;
+    if (event.keyCode === 13) {
+      fetch(geoUrl)
+        .then((res) => res.json())
+        .then((res) => {
+          const { lat, lon } = res[0];
+          return "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely&appid=b215978ae0b5adea831c87cd99ac6d51"
+        })
+        .then(url => {
+          fetch(url)
+          .then(res => console.log(res.json()))
+        })
+    }
+  };
+
+  return (
+    <input className="searchBar" autoFocus={true} onKeyUp={search}></input>
+  );
 }
 
 export default SearchBar;
