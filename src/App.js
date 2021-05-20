df68f2dc53ea8b8dea42f665194029acee861c90
nguyen <>
2021-05-20 23:15:13 +0900

    update url

--- src/App.js
+++ src/App.js
@@ -10,57 +10,63 @@ import { useState, useEffect } from "react";
 function App() {
   const [data, updateData] = useState({})
   const [isLoaded, updateStatus] = useState(false)
-  async function fetchWeather(){
-   const geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=fukuoka&appid=b215978ae0b5adea831c87cd99ac6d51";
+  async function fetchWeather() {
+    const geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=fukuoka&appid=b215978ae0b5adea831c87cd99ac6d51";
     const response = await fetch(geoUrl)
     const coordinates = await response.json()
-    
+
     const { lat, lon } = coordinates[0]
-    
-   const url =  "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely&appid=b215978ae0b5adea831c87cd99ac6d51"
+
+    const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + 
+    "&lon=" + lon + 
+    "&exclude=minutely&appid=b215978ae0b5adea831c87cd99ac6d51&lang=ja&units=metric"
     const result = await fetch(url)
     const weatherData = await result.json()
-    .then(res => res)
 
-    if(!response.ok || !result.ok){
+    if (!response.ok || !result.ok) {
       const message = `Error: ${response.status || result.status}`
       throw new Error(message)
     }
     return weatherData
-  
+
   }
 
   useEffect(() => {
     fetchWeather()
-    .then(res => {
-      updateData(res)
-      updateStatus(true)
-    })
-    .catch(err => err.message)
+      .then(res => {
+        updateData(res)
+        updateStatus(true)
+      })
+      .catch(err => err.message)
 
   }, [])
 
 
-  
-  if(isLoaded){
-  return (
-    <div className="App">
-     
-      <div className='container'>
-      <SearchBar />
-      <WeatherCard content={data} />
-      <SearchHistory />
-      <DaysBar/>
-      <HoursSlider/>
-      <MainDisplay/>
+
+  if (isLoaded) {
+    return (
+      <div className="App">
+
+        <div className='container'>
+          <SearchBar />
+          <WeatherCard content={data} />
+          <SearchHistory />
+          <DaysBar />
+          <HoursSlider />
+          <MainDisplay />
+
+        </div>
 
       </div>
+    )
+  }else{
+    return (
+      <div>
+        <h1 className="App">Loading...</h1>
+      </div>
+    )
+  }
 
-    </div>
-  );
-}else{
-  return null
-}
 }
 
 export default App;
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,16 +1,15 @@
 import React from "react"
 
 function WeatherCard(props) {
-   const data = props.content
-   const current = data.current
-   console.log(current)
+   const content = props.content
+   const current = content.current
 
    return (
       <div className="cardContainer">
       <div className='weatherCard'>
          <h2 className='location'>Fukuoka</h2>
          <p className='status'>{current.weather[0].description}</p>
-         <h1 className='temp'>19</h1>
+         <h1 className='temp'>{current.temp}º</h1>
       </div>
       <div className='weatherCard'>
          <h2 className='location'>Fukuoka</h2>
