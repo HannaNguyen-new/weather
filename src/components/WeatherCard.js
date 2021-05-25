9216c18167a04735281e9efe489509203b1959e2
nguyen <>
2021-05-26 07:53:36 +0900

    second commit

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function WeatherCard(props) {
    const current = props.card1.current
-   const card2 = props.card2.current
+   //const card2 = props.card2.current || {}
 
    return (
       <div className="cardContainer">
@@ -12,9 +12,9 @@ function WeatherCard(props) {
          <h1 className='temp'>{current.temp}º</h1>
       </div>
       <div className='weatherCard' id='card2'>
-         <h2 className='location'>{props.location2.name}</h2>
-         <p className='status'>{card2.weather[0].description}</p>
-         <h1 className='temp'>{card2.temp}º</h1>
+         <h2 className='location'>another location</h2>
+         <p className='status'> no info</p>
+         <h1 className='temp'>º</h1>
       </div>
 
       </div>
