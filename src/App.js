dccf66af54aae2f301cc22207942e85ad8ba3001
nguyen <hanchan1305@gmail.com>
2021-05-31 06:17:01 +0900

    refactor app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,45 @@ import MainDisplay from "./components/MainDisplay";
 import SearchBar from "./components/SearchBar";
 import SearchHistory from "./components/SearchHistory";
 import WeatherCard from "./components/WeatherCard";
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useCallback } from "react";
+
+// move functions outside component so that i don't need to include them in dependencies array
+// these functions do not reference any props or states
+const findNearestCity = async(lat,lon) => {
+  const url = "http://api.openweathermap.org/geo/1.0/reverse?lat=" + lat +
+  "&lon=" + lon + "&limit=5&appid=" + config.openAPI_key
+  const result = await fetch(url)
+ const nearestCities = await result.json()
+ if (!result.ok) {
+ const message = `Error: ${result.status}`
+ throw new Error(message)
+ }
+ return nearestCities
+ }
+
+ const fetchWeather = async(lat,lon) => {
+
+   const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + 
+   "&lon=" + lon + "&exclude=minutely&appid=" + config.openAPI_key + '&units=metric'
+   const result = await fetch(url)
+   const weatherData = await result.json()
+
+   if (!result.ok) {
+     const message = `Error: ${result.status}`
+     throw new Error(message)
+   }
+   return weatherData
+
+ }
+
+ const fetchAll = async(lat,lon) => {
+   const promise1 = findNearestCity(lat, lon);
+   const promise2 = fetchWeather(lat, lon);
+   Promise.all([promise1,promise2])
+   
+ }
+
+ 
 
 function App() {
 
@@ -18,113 +56,73 @@ function App() {
   // const utc = Date.UTC(year,month-1,date,hour)
   // const epoch = Math.floor(utc/1000) 
 
-  const history = [];
+  const [history, updateHistory] = useState([])
   const [currentCoords,updateCurrentCoords] = useState('')
-  //const [coordsFetched, updateFetchStatus] = useState(false)
-  const [locationName, updateLocationName] = useState("")
   const [card1, updateCard1] = useState({location:'', weather:''})
   const [card2, updateCard2] = useState({location:'', weather:''})
-  //const [isLoaded, updateStatus] = useState(false)
   const [locationId, updateLocationId] = useState("")
-  //const [secondCoords, updateSecondCoords] = useState("")
-  //const [secondCoordsFetched, updateSecondStatus] = useState("")
-  //const [secondLocationName, updateSecondLocationName] = useState(false)
-  //const [card2, updateCard2] = useState("")
+  const [firstLoad, updateFirstLoad] = useState(false)
 
 
-  useEffect(() => {
-    if(locationId.length > 0) {
-     getCoords()
-      .then(result => {
-        //updateSecondCoords(result.position)
-        //updateSecondStatus(true)
-        const {lat,lng : lon} = result.position
-        fetchAll(lat,lon)
-        .then(values =>  updateCard2({location: values[0][1], weather: values[1]})
-        )
-         .catch(err => err.message)
-      })
-      .catch(err => console.log(err))
-
-    }
-
-    
-  },[locationId])
+  
 
   const getLocationId = (id) => {
     updateLocationId(id)
   }
-  const getCoords = async() => {
-    const url = 'https://lookup.search.hereapi.com/v1/lookup?apiKey=' + config.hereAPI_key + '&id=' + locationId ;
-    const result = await fetch(url)
-    const coords =  result.json()
-    return coords
   
-  }
-  const getCurrentCoords = async() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      updateCurrentCoords(position.coords);
-      //updateFetchStatus(true)
-    })
-  }
+  const getCoords = useCallback(
+    async() => {
+      const url = 'https://lookup.search.hereapi.com/v1/lookup?apiKey=' + config.hereAPI_key + '&id=' + locationId ;
+      const result = await fetch(url)
+      const coords =  result.json()
+      return coords
+    }, [locationId]) 
+  
 
-  const findNearestCity = async(lat,lon) => {
-   const url = "http://api.openweathermap.org/geo/1.0/reverse?lat=" + lat +
-   "&lon=" + lon + "&limit=5&appid=" + config.openAPI_key
-   const result = await fetch(url)
-  const nearestCities = await result.json()
-  if (!result.ok) {
-  const message = `Error: ${result.status}`
-  throw new Error(message)
-  }
-  return nearestCities
-  }
-
-  const fetchWeather = async(lat,lon) => {
-
-    const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + 
-    "&lon=" + lon + "&exclude=minutely&appid=" + config.openAPI_key + '&units=metric'
-    const result = await fetch(url)
-    const weatherData = await result.json()
-
-    if (!result.ok) {
-      const message = `Error: ${result.status}`
-      throw new Error(message)
-    }
-    return weatherData
-
-  }
-
-  const fetchAll = async(lat,lon) => {
-    const promise1 = findNearestCity(lat, lon);
-    const promise2 = fetchWeather(lat, lon);
-    Promise.all([promise1,promise2])
-    
-  }
   useEffect(()=> {
+    console.log("i run first")
+    const getCurrentCoords = async() => {
+      navigator.geolocation.getCurrentPosition(position => {
+        updateCurrentCoords(position.coords);
+      })
+    }
+
     getCurrentCoords();
     if(currentCoords){
       const {latitude: lat, longitude: lon} = currentCoords;
       fetchAll(lat,lon)
       .then(values => {
         updateCard1({location:values[0][1], weather:values[1]});
-        //updateStatus(true)
+        updateFirstLoad(true)
       })
       .catch(err => err.message)
-      history.push(card1)
+      updateHistory(currentHistory => currentHistory.push(card1))
     }
 
-  },[currentCoords])
+  },[currentCoords, card1, firstLoad, history])
   
+ useEffect(() => {
+    console.log("i run second")
+    if(locationId.length > 0) {
+     getCoords()
+      .then(result => {
+        const {lat,lng : lon} = result.position
+        fetchAll(lat,lon)
+        .then(values =>  updateCard2({location: values[0][1], weather: values[1]})
+        )
+      })
+      .catch(err => err.message)
 
+    }
+  },[locationId, getCoords])
 
-  if (isLoaded) {
+  if (firstLoad) {
     return (
       <div className="App">
 
         <div className='container'>
           <SearchBar getId={getLocationId} />
-          <WeatherCard card1={card1} location1={locationName} card2={card2} location2={secondLocationName} />
+          <WeatherCard card1={card1}  card2={card2} />
           <SearchHistory />
           <DaysBar />
           <HoursSlider />
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    openAPI_key : 'b215978ae0b5adea831c87cd99ac6d51',
-    hereAPI_key : 'SIa3lD9mGs3uMTxN9OEZlk7FRZRKbbPwfEAv8D_8QHs'
+    openAPI_key : '8324dbbddd8c75efc68674b50dc3dbc5',
+    hereAPI_key : 'SIa3lD9mGs3uMTxN9OEZlk7FRZRKbbPwfEAv8D_8QHs',
+    mapsAPI_key : 'AIzaSyDhKOX7YRtG_8aNN4RTIoGKrWWMDi9FzXU'
 }
 export default config
